73eaf44f383ed1590e16dc1d4fc3a59fb382413b
AdrishK03 <adrishkarmakar712149@gmail.com>
2025-10-28 19:57:19 +0530

    Update entrypoint.sh

--- HMS/entrypoint.sh
+++ HMS/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Run database setup (db.create_all() and admin insert)
-python -c "from app import setup_database; setup_database()"
+python -c "from app import init_db; init_db()"
 # Start the production web server
 gunicorn app:app
